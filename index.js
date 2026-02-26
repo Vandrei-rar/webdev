213df6ea806f21bb6a4097d33471dca0f71eb151
Vandrei Ribeiro <vandrei.ribeiro@informa.solutions>
2026-02-25 21:11:57 -0300

    atualizando código

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require("express")
+const { engine } = require("express-handlebars")
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use("/", (req, res) => {
-    res.send("<h1>Funcionou</h1>")
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.engine('handlebars', engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.get("/", (req, res) => {
+    res.render('index')
+})
+
+app.get("/sobre/{:id}", (req, res) => {
+
+    const id = req.params.id
+    
+    res.render("sobre", { id })
 })
 
 app.listen(3000, () => {
